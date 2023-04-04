2ddd7f7c44dec4514b9f6faf1bba635ffff2b488
rohan28107 <rohangajre21@gmail.com>
2023-04-04 15:14:49 +0530

    adding M12 server 5

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -64,6 +64,36 @@ userRouter.post('/login', async (req, res) => {
     }
 });
 
+
+userRouter.post('/calculate', async(req, res) =>{
+    try {
+        const { annualInstalmentAmount, annualInterestRate, totalNumberOfYears } = req.body;
+        const rateOfInterest = annualInterestRate / 100;
+        const totalNumberOfMonths = totalNumberOfYears * 12;
+        const maturityValue = annualInstalmentAmount * ((Math.pow((1 + rateOfInterest), totalNumberOfMonths) - 1) / rateOfInterest);
+        const totalInvestmentAmount = annualInstalmentAmount * totalNumberOfYears;
+        const totalInterestGained = maturityValue - totalInvestmentAmount;
+
+        const calculation = new Calculation({
+            annualInstalmentAmount,
+            annualInterestRate,
+            totalNumberOfYears,
+            totalInterestGained,
+            totalInvestmentAmount,
+            maturityValue
+        })
+        await calculation.save();
+        return res.json({
+            totalInvestmentAmount,
+            totalInterestGained,
+            maturityValue
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal Server error', error: error.message });
+    }
+})
+
 // userRouter.post('/register', async (req, res) => {
 //     try{
 //         const { name, email, password } = req.body;
