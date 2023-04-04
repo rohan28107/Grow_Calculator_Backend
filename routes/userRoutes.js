32834e9ad7a69ed4ccf8a6dadbc0a86258b1b32e
rohan28107 <rohangajre21@gmail.com>
2023-04-04 15:15:21 +0530

    adding M12 server 6

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -164,24 +164,24 @@ userRouter.get('/profile', async (req, res) => {
     }
 });
 
-userRouter.post('/calculate', async(req, res) =>{
-    try {
-        const { annualInstalmentAmount, annualInterestRate, totalNumberOfYears } = req.body;
-        const rateOfInterest = annualInterestRate / 100;
-        const totalNumberOfMonths = totalNumberOfYears * 12;
-        const maturityValue = annualInstalmentAmount * ((Math.pow((1 + rateOfInterest), totalNumberOfMonths) - 1) / rateOfInterest);
-        const totalInvestmentAmount = annualInstalmentAmount * totalNumberOfYears;
-        const totalInterestGained = maturityValue - totalInvestmentAmount;
-        return res.json({
-            totalInvestmentAmount,
-            totalInterestGained,
-            maturityValue
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Internal Server error', error: error.message });
-    }
-})
+// userRouter.post('/calculate', async(req, res) =>{
+//     try {
+//         const { annualInstalmentAmount, annualInterestRate, totalNumberOfYears } = req.body;
+//         const rateOfInterest = annualInterestRate / 100;
+//         const totalNumberOfMonths = totalNumberOfYears * 12;
+//         const maturityValue = annualInstalmentAmount * ((Math.pow((1 + rateOfInterest), totalNumberOfMonths) - 1) / rateOfInterest);
+//         const totalInvestmentAmount = annualInstalmentAmount * totalNumberOfYears;
+//         const totalInterestGained = maturityValue - totalInvestmentAmount;
+//         return res.json({
+//             totalInvestmentAmount,
+//             totalInterestGained,
+//             maturityValue
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ message: 'Internal Server error', error: error.message });
+//     }
+// })
 
 module.exports = {
     userRouter
