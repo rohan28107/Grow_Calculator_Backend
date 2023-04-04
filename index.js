ccf2e56079622aa6a6c83784dad6cc6e03ffd13b
rohan28107 <rohangajre21@gmail.com>
2023-04-04 15:24:12 +0530

    adding M12 server 10

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(bodyParser.json());
+// app.use(bodyParser.json()); ase
 
 app.use('/users', userRouter);
 app.use(authenticate);
